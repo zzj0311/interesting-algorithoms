8ef43ed1ea2d060a00273ef422df8ca1ed106479
zzzzzzj <zhuangzijun1996@zju.edu.cn>
2017-04-02 17:26:17 +0800

    finally restore the lost code

--- sol3/sol3.py
+++ sol3/sol3.py
@@ -1,5 +1,5 @@
 class Hero:
-    def __init__(self, HP = 0, AP = 0, BUFF = 0, pos_x = 0, pos_y = 0):
+    def __init__(self, HP = 0, AP = 0, BUFF = 0, pos_x = 0, pos_y = 0):#pos_x 为行数，pos_y 为列数（just a reminder
         self.HP = HP
         self.AP = AP
         self.BUFF = BUFF
@@ -15,11 +15,46 @@ class Monster:
         self.pos_y = pos_y
 
 class cell:
-    def __init__(self, Class = 0, A_monster = Monster(), hasMonster = False):
+    def __init__(self, Class = 0, A_monster = Monster(), hasMonster = False, Monster_num = 20):
         self.Class = Class
         self.A_monster = A_monster
         self.hasMonster = hasMonster
+        self.Monster_num = Monster_num
 
+class Game:
+    def __init__(self, maze = [], Monsters = [], Hero = Hero()):
+        self.maze = maze
+        self.Monsters = Monsters
+        self.Hero = Hero
+
+    def Move_left(self):
+        for i in reversed(range(self.Hero.pos_y)):
+            if self.maze[self.Hero.pos_x][i].Class != 0:
+                self.Hero.pos_y = i
+            return
+
+    def Move_right(self):
+        for i in range(self.Hero.pos_y + 1, len(self.maze[0])):
+            if self.maze[self.Hero.pos_x][i].Class != 0:
+                self.Hero.pos_y = i
+            return
+
+    def Move_up(self):
+        for j in reversed(range(self.Hero.pos_x)):
+            if self.maze[j][self.Hero.pos_y].Class != 0:
+                self.Hero.pos_y = i
+            return
+
+    def Move_down(self):
+        for j in range(self.Hero.pos_x + 1, len(self.maze)):
+            if self.maze[j][self.Hero.pos_y].Class != 0:
+                self.Hero.pos_y = i
+            return
+
+class Search_tree:
+    def __init__(self, child = [], status = Game()):
+        self.child = child
+        self.status = status
 def block_distance(The_Hero, A_Monster):
     return abs(The_Hero.pos_x + The_Hero.pos_y - A_Monster.pos_x - A_Monster.pos_y)
 
@@ -34,15 +69,15 @@ for i in range(N):
     for j in range(M):
         if input_line[j] == "S":
             Monster_list.append(Monster(hasBuff = True, pos_x = i, pos_y = j))
-            line.append(cell(1,Monster_list[-1],True))
+            line.append(cell(2, Monster_list[-1], True, len(Monster_list) - 1))
         elif input_line[j] == "M":
             Monster_list.append(Monster(hasBuff = False, pos_x = i, pos_y = j))
-            line.append(cell(2,Monster_list[-1],True))
+            line.append(cell(2, Monster_list[-1], True, len(Monster_list) - 1))
         elif input_line[j] == "D":
             line.append(cell())
             Little_HI = Hero(BUFF = 5, pos_x = i, pos_y = j)
         else:
-            line.append(cell())
+            line.append(cell(Class = 1))
     Maze.append(line)
     line = []
 for monster in Monster_list:
@@ -52,3 +87,4 @@ for monster in Monster_list:
 hero_attrs = input().split(" ")
 Little_HI.HP = hero_attrs[0]
 Little_HI.AP = hero_attrs[1]
+Initial_game = Game(Maze, Monster_list, Little_HI)
